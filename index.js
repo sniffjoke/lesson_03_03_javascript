19b68f1c7f63c34f3a2ee3f7c0a090792e4d549b
sniffjoke <sniffjoke@gmail.com>
2024-09-23 18:07:49 +0200

    add static

--- index.js
+++ index.js
@@ -15,42 +15,62 @@
 // console.log(bmw)
 // console.log(kia)
 
-class Car {
+// class Car {
+//
+//     #speed = 100
+//     #wheels = 4
+//
+//     constructor(brand, color) {
+//         this.brand = brand;
+//         this.color = color;
+//     }
+//
+//     startEngine() {                             // создается один раз
+//         console.log(`${this.brand} started`);
+//     }
+//
+//     #validateAsNumber(speed) {
+//         if (typeof speed !== "number") {
+//             throw new Error(`Invalid speed: ${speed}`);
+//         }
+//     }
+//
+//     // startEngine = function() {
+//     //     console.log(`${this.brand} started`);           // создается на каждом новом экземпляре
+//     // }
+//
+//     set setSpeed(speed) {
+//         this.#validateAsNumber(speed);
+//         this.#speed = speed;
+//     }
+//
+//     get speed() {
+//         return this.#speed;
+//     }
+//
+//     static compareCarSpeed(car1, car2) {
+//         if (car1.speed > car2.speed) {
+//             console.log(`${car1.brand} is faster`)
+//         }
+//         if (car2.speed > car2.speed) {
+//             console.log(`${car2.brand} is faster`)
+//         }
+//     }
+//
+// }
 
-    #speed
-
-    constructor(brand, color) {
-        this.#speed = 100
-        this.brand = brand;
-        this.color = color;
-    }
-
-    startEngine() {                             // создается один раз
-        console.log(`${this.brand} started`);
-    }
-
-    // startEngine = function() {
-    //     console.log(`${this.brand} started`);           // создается на каждом новом экземпляре
-    // }
-
-    set speed(speed) {
-        this.#speed = speed;
-    }
-
-    get speed() {
-        return this.#speed;
-    }
-
-}
-
-const bmw = new Car('bmw', 'white');
-const kia = new Car('kia', 'red');
+// const bmw = new Car('bmw', 'white');
+// const kia = new Car('kia', 'red');
 
 // bmw.startEngine();
 
-// console.log(bmw)
 // console.log(kia)
 
-console.log(bmw.speed);
-bmw.speed(123)
+// bmw.startEngine();
 // console.log(bmw.speed);
+// bmw.setSpeed = 123
+// console.log(bmw.speed);
+
+// Car.compareCarSpeed(bmw, kia)
+
+
