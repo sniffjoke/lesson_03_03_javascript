6c85e674bc21cfe416a696b78ed99014a189f8e1
sniffjoke <sniffjoke@gmail.com>
2024-09-23 17:55:26 +0200

    add get set

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-function CarCreate(brand, color) {
-    this.brand = brand;
-    this.color = color;
-}
+// function CarCreate(brand, color) {
+//     this.brand = brand;
+//     this.color = color;
+// }
 
-CarCreate.prototype.startEngine = function () {
-    console.log(`${this.brand} started`);
-};
+// CarCreate.prototype.startEngine = function () {
+//     console.log(`${this.brand} started`);
+// };
 
-const bmw = new CarCreate('bmw', 'white');
-const kia = new CarCreate('kia', 'red');
+// const bmw = new CarCreate('bmw', 'white');
+// const kia = new CarCreate('kia', 'red');
 
 // bmw.startEngine();
 // kia.startEngine();
-console.log(bmw)
-console.log(kia)
+// console.log(bmw)
+// console.log(kia)
+
+class Car {
+
+    #speed
+
+    constructor(brand, color) {
+        this.#speed = 100
+        this.brand = brand;
+        this.color = color;
+    }
+
+    startEngine() {                             // создается один раз
+        console.log(`${this.brand} started`);
+    }
+
+    // startEngine = function() {
+    //     console.log(`${this.brand} started`);           // создается на каждом новом экземпляре
+    // }
+
+    set speed(speed) {
+        this.#speed = speed;
+    }
+
+    get speed() {
+        return this.#speed;
+    }
+
+}
+
+const bmw = new Car('bmw', 'white');
+const kia = new Car('kia', 'red');
+
+// bmw.startEngine();
+
+// console.log(bmw)
+// console.log(kia)
+
+console.log(bmw.speed);
+bmw.speed(123)
+// console.log(bmw.speed);
